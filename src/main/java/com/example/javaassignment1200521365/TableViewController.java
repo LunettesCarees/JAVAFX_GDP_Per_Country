d21cd4ff85dee4011ce59daaa5cb2401f0938e99
Guanzhou Chen <guanzhouchen11@gmail.com>
2023-06-15 21:01:04 -0400

    Modified table view and its controller

--- src/main/java/com/example/javaassignment1200521365/TableViewController.java
+++ src/main/java/com/example/javaassignment1200521365/TableViewController.java
@@ -1,4 +1,46 @@
 package com.example.javaassignment1200521365;
 
-public class TableViewController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TableViewController implements Initializable {
+    @FXML
+    private TableView<Country> tableView;
+
+    @FXML
+    private TableColumn<Country, Integer> idColumn;
+
+    @FXML
+    private TableColumn<Country, String> countryColumn;
+
+    @FXML
+    private TableColumn<Country, Integer> GDP2017Column;
+
+    @FXML
+    private TableColumn<Country, Integer> GDP2018Column;
+
+    @FXML
+    private TableColumn<Country, Integer> GDP2019Column;
+
+    @FXML
+    private TableColumn<Country, Integer> GDP2020Column;
+
+    @FXML
+    private TableColumn<Country, Integer> GDP2021Column;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
+
+    @FXML
+    void viewChart_onClick(ActionEvent event) {
+
+    }
+
 }
