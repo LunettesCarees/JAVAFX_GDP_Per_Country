8923faf5646667a2d91a0f8137e118a4eb85c329
Guanzhou Chen <guanzhouchen11@gmail.com>
2023-06-15 21:56:25 -0400

    Modified ChartViewController

--- src/main/java/com/example/javaassignment1200521365/ChartViewController.java
+++ src/main/java/com/example/javaassignment1200521365/ChartViewController.java
@@ -2,14 +2,32 @@ package com.example.javaassignment1200521365;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class ChartViewController {
+public class ChartViewController implements Initializable {
+    @FXML
+    private CategoryAxis categoryAxis;
+
+    @FXML
+    private LineChart<String, Integer> lineChart;
+
+    @FXML
+    private NumberAxis numberAxis;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 
     @FXML
     void viewTable_onClick(ActionEvent event) throws IOException {
         SceneChanger.changeScenes(event, "table-view.fxml", "Table View");
     }
-
 }
