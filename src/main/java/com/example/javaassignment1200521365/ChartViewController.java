00bfc7d33391035603da794dd8db8bb0ea58c4a3
Guanzhou Chen <guanzhouchen11@gmail.com>
2023-06-15 23:02:52 -0400

    Create a line chart to randomly show at max 8 countries' GDPs

--- src/main/java/com/example/javaassignment1200521365/ChartViewController.java
+++ src/main/java/com/example/javaassignment1200521365/ChartViewController.java
@@ -6,9 +6,12 @@ import javafx.fxml.Initializable;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.ResourceBundle;
 
 public class ChartViewController implements Initializable {
@@ -23,11 +26,42 @@ public class ChartViewController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        int count = 0;
 
+        categoryAxis.setLabel("Year");
+        numberAxis.setLabel("GDP (millions in USD)");
+        lineChart.setTitle("GDP By Country From 2017 to 2021");
+
+        ArrayList<Country> countries = DBUtility.retrieveCountriesFromDB();
+
+        for (Country country : countries) {
+            // Create 9 random series
+            Random random = new Random();
+            int r = random.nextInt(countries.size() / 9);
+            if (r != 0) {
+                continue;
+            }
+            XYChart.Series<String, Integer> series = new XYChart.Series<>();
+            series.setName(country.getCountryName());
+            series.getData().add(new XYChart.Data<>("2017", country.getGDP2017()));
+            series.getData().add(new XYChart.Data<>("2018", country.getGDP2018()));
+            series.getData().add(new XYChart.Data<>("2019", country.getGDP2019()));
+            series.getData().add(new XYChart.Data<>("2020", country.getGDP2020()));
+            series.getData().add(new XYChart.Data<>("2021", country.getGDP2021()));
+            lineChart.getData().addAll(series);
+            count++;
+            if (count == 8) {
+                break;
+            }
+        }
     }
 
     @FXML
     void viewTable_onClick(ActionEvent event) throws IOException {
         SceneChanger.changeScenes(event, "table-view.fxml", "Table View");
+    }
+
+    public LineChart<String, Integer> getLineChart() {
+        return lineChart;
     }
 }
